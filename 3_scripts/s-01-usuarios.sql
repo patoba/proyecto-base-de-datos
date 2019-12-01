1baf637eac59008a13a1ee762674f55955f413de
Emanuel Flores <mufymes@gmail.com>
2019-12-01 12:09:30 -0600

    feat: Created users and roles

---
+++ 3_scripts/s-01-usuarios.sql
@@ -0,0 +1,23 @@
+--@Autores: Emanuel Flores, Adolfo Barrero
+--@Fecha creación: 01/12/2019
+--@Descripción: Usuarios y roles del proyecto
+
+prompt Creando a los usuarios
+create user fb_proy_admin indentified by asdf quota unlimited on users;
+
+create user fb_proy_invitado identified by asdf;
+
+prompt Creando los roles
+create role rol_admin;
+
+grant create session, create synonym, create view, create table,
+create sequence, create trigger, create procedure to rol_admin;
+
+create role rol_invitado;
+
+grant create session to rol_invitado;
+
+prompt Asignando roles
+grant rol_admin to fb_proy_admin;
+
+grant rol_invitado to fb_proy_invitado;
