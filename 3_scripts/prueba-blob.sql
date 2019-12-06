8e3365e6a043045d6673ee1d943823e33804d770
Emanuel Flores <mufymes@gmail.com>
2019-12-06 15:57:57 -0600

    Script 17 completo creo xd

---
+++ 3_scripts/prueba-blob.sql
@@ -0,0 +1,94 @@
+create or replace procedure guarda_objeto_blob (
+    v_nombre_directorio in varchar2,
+    v_nombre_archivo    in varchar2,
+    v_nombre_tabla      in out varchar2,
+    v_nombre_col_blob   in out varchar2,
+    v_nombre_col_pk     in out varchar2,
+    v_valor_pk          in number,
+    v_longitud          out number) is
+
+    v_sql varchar2(2000);
+    v_blob blob;
+    v_file utl_file.FILE_TYPE;
+    v_buffer_size number := 32767;
+    v_buffer RAW(32767);
+    v_position number := 1;
+begin
+    v_nombre_tabla := dbms_assert.simple_sql_name(v_nombre_tabla);
+    v_nombre_col_blob := dbms_assert.simple_sql_name(v_nombre_col_blob);
+    v_nombre_col_pk := dbms_assert.simple_sql_name(v_nombre_col_pk);
+
+    v_sql := 'select ' || v_nombre_col_blob
+             || ' into :ph_blob'
+             || ' from '  || v_nombre_tabla
+             || ' where ' || v_nombre_col_pk
+             || ' = :ph_pk';
+
+    execute immediate v_sql into v_blob using v_valor_pk; 
+
+    v_file := utl_file.fopen(upper(v_nombre_directorio), v_nombre_archivo, 'wb', v_buffer_size);
+
+    v_longitud := dbms_lob.getlength(v_blob);
+
+    while v_position < v_longitud loop
+        dbms_lob.read(v_blob, v_buffer_size, v_position, v_buffer);
+        utl_file.put_raw(v_file, v_buffer, true);
+        v_position := v_position + v_buffer_size;
+    end loop;
+
+    utl_file.fclose(v_file);
+
+exception
+    when others then 
+        if utl_file.is_open(v_file) then
+            utl_file.fclose(v_file);
+        end if;
+
+        dbms_output.put_line(dbms_utility.format_error_backtrace);
+
+        v_longitud := -1;
+        raise;
+end;
+/
+show errors
+
+create or replace trigger guarda_empleado_firma
+after insert or update on empleado
+for each row
+declare
+    v_blob blob;
+    v_nombre_directorio varchar2(30) := 'DATA_DIR';
+    v_nombre_archivo varchar2(30);
+    v_file utl_file.FILE_TYPE;
+    v_buffer_size number := 32767;
+    v_buffer RAW(32767);
+    v_position number := 1;
+    v_longitud number(10,0);
+begin
+    v_blob := :new.firma;
+    v_nombre_archivo := 'e_' || to_char(:new.empleado_id) || '.jpg';
+    v_file := utl_file.fopen(upper(v_nombre_directorio), v_nombre_archivo, 'wb', v_buffer_size);
+
+    v_longitud := dbms_lob.getlength(v_blob);
+
+    while v_position < v_longitud loop
+        dbms_lob.read(v_blob, v_buffer_size, v_position, v_buffer);
+        utl_file.put_raw(v_file, v_buffer, true);
+        v_position := v_position + v_buffer_size;
+    end loop;
+
+    utl_file.fclose(v_file);
+
+exception
+    when others then 
+        if utl_file.is_open(v_file) then
+            utl_file.fclose(v_file);
+        end if;
+
+        dbms_output.put_line(dbms_utility.format_error_backtrace);
+
+        v_longitud := -1;
+        raise;
+end;
+/
+show errors
---
+++ 3_scripts/prueba_blob2.sql
@@ -0,0 +1,40 @@
+create or replace trigger guarda_objeto_blob_2
+after insert or update on empleado
+for each row
+declare
+    v_blob blob;
+    v_nombre_directorio varchar2(30) := 'DATA_DIR';
+    v_nombre_archivo varchar2(30);
+    v_file utl_file.FILE_TYPE;
+    v_buffer_size number := 32767;
+    v_buffer RAW(32767);
+    v_position number := 1;
+    v_longitud number(10,0);
+begin
+    v_blob := :new.firma;
+    v_nombre_archivo := to_char(:new.empleado_id);
+    v_file := utl_file.fopen(upper(v_nombre_directorio), v_nombre_archivo, 'wb', v_buffer_size);
+
+    v_longitud := dbms_lob.getlength(v_blob);
+
+    while v_position < v_longitud loop
+        dbms_lob.read(v_blob, v_buffer_size, v_position, v_buffer);
+        utl_file.put_raw(v_file, v_buffer, true);
+        v_position := v_position + v_buffer_size;
+    end loop;
+
+    utl_file.fclose(v_file);
+
+exception
+    when others then 
+        if utl_file.is_open(v_file) then
+            utl_file.fclose(v_file);
+        end if;
+
+        dbms_output.put_line(dbms_utility.format_error_backtrace);
+
+        v_longitud := -1;
+        raise;
+end;
+/
+show errors
