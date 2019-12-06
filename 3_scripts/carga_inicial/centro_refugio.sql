477ae2ad5ddb2dc2ada8d361851d7385168b64db
patoba <patobarrero@gmail.com>
2019-12-05 19:59:00 -0600

    carga inicial

---
+++ 3_scripts/carga_inicial/centro_refugio.sql
@@ -0,0 +1,4 @@
+insert into centro_operativo (centro_operativo_id, numero_registro, capacidad, lema, logo) values (4, 7974776319, 781, 'Secured', lee_imagen('logo_4.png'));
+insert into centro_operativo (centro_operativo_id, numero_registro, capacidad, lema, logo) values (11, 1036816544, 22, 'Exclusive', lee_imagen('logo_11.png'));
+insert into centro_operativo (centro_operativo_id, numero_registro, capacidad, lema, logo) values (14, 4922061820, 304, '6th generation', lee_imagen('logo_14.png'));
+insert into centro_operativo (centro_operativo_id, numero_registro, capacidad, lema, logo) values (15, 3339455803, 395, 'focus group', lee_imagen('logo_15.png'));
