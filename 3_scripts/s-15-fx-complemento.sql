a88973d608e2abab14133aeaeb46881df75b8ca3
Emanuel Flores <mufymes@gmail.com>
2019-12-07 17:51:57 -0600

    Funcion complemento completa

--- 3_scripts/s-15-fx-complemento.sql
+++ 3_scripts/s-15-fx-complemento.sql
@@ -1,25 +1,31 @@
 set serveroutput on
 
 Prompt creando procedimiento para insertar un archivo.
-create or replace function complemento(p_cadena in varchar2, p_n in number) 
-return varchar2 is v_completa varchar2(10);
-    
+create or replace function complemento(p_cadena in varchar2, p_numero in number) 
+return varchar2 is 
+    v_completa varchar2(10);
+    v_longitud number;
 begin
-    v_longitud integer;
 
-    if length(p_cadena) >= p_n then
-        v_completa := p_cadena;
-        return(v_completa);
-    end if;
-
-    v_longitud := p_n - length(p_cadena);
+    v_longitud := p_numero - length(p_cadena) - 1;
+    v_completa := p_cadena;
 
     for i in 0..v_longitud loop
-        v_completa := '0' || p_cadena;
+        v_completa := '0' || v_completa;
     end loop;
 
-    return(v_completa);
+    return v_completa;
 end;
 /
 show errors
 
+set serveroutput on
+declare 
+    v_cadena varchar2(10) := '678A';
+    v_numero number := 10;
+    v_dato varchar2(10);
+begin
+    v_dato := complemento(v_cadena, v_numero);
+    dbms_output.put_line('Cadena: ' || v_dato);
+end;
+/
